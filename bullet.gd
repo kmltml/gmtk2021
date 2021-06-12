da12c081d6d47054d888301823880dcc9cf0b02b
kmltml <kamil.tomala@gmail.com>
2021-06-12 12:22:04 +0200

    Make bullets kill enemies

--- bullet.gd
+++ bullet.gd
@@ -4,10 +4,17 @@ export var speed = Vector2(0.0, 1.0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass # Replace with function body.
-
+    connect("body_entered", self, "on_body_entered")
+    update_rotation()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    self.rotation = atan2(speed.x, -speed.y)
+    update_rotation()
     self.position += speed * delta
+
+func on_body_entered(body):
+    body.queue_free()
+    queue_free()
+
+func update_rotation():
+    self.rotation = atan2(speed.x, -speed.y)
